bb6feb4e418d26f758bf1bece133e5b03cc4d96c
Sappakit <pipitwattana.s@gmail.com>
2025-02-24 18:01:22 +0700

    feat: set up project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <p className="text-7xl text-red-500">Test</p>
+    </div>
+  );
+}
+
+export default App;
